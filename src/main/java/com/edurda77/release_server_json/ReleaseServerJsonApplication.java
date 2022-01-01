6bc727f2868a9517283b06bd9fc5f94c6ff7895e
Эдуард <edurd@rambler.ru>
2022-01-01 19:46:05 +0500

    Initial commit

---
+++ src/main/java/com/edurda77/release_server_json/ReleaseServerJsonApplication.java
@@ -0,0 +1,13 @@
+package com.edurda77.release_server_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseServerJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseServerJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edurda77/release_server_json/ReleaseServerJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edurda77.release_server_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseServerJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
